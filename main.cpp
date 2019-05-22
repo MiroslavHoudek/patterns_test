a1f440a7cf857caac362618b3db00b9ce78e8fe5
Miroslav Houdek <miroslav.houdek@gmail.com>
2019-05-22 16:40:18 +0200

    Initial commit, it's working but can't unregister

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+
+#include "observable.h"
+
+class Callback : public Observable<
+                            void(void),
+                            void(int,std::string),
+                            void(int,int)
+                            >
+{
+public:
+
+private:
+    int a;
+    std::string s;
+};
+
+class ReceiverA
+{
+public:
+    void EventZeroHappened()
+    {
+        std::cout << "A has observed event zero\n";
+    }
+
+    void EventOneHappened(int a, std::string s)
+    {
+        std::cout << "A has observed event one with params: " << a << " and " << "\"" << s << "\"" <<'\n';
+    }
+
+    void EventTwoHappened(int a, int b)
+    {
+        std::cout << "A has observed event two with params: " << a << " and " << b <<'\n';
+    }
+};
+
+class ReceiverB
+{
+public:
+    void EventZeroHappened()
+    {
+        std::cout << "B has observed event zero\n";
+    }
+};
+
+int main()
+{
+    Callback cb;
+    ReceiverA r1;
+    ReceiverB r2;
+
+    cb.registerCallback<0>([&r1](){r1.EventZeroHappened();});
+    cb.registerCallback<1>([&r1](int i, std::string s){r1.EventOneHappened(i,s);});
+    cb.registerCallback<2>([&r1](int i, int j){r1.EventTwoHappened(i,j);});
+
+    cb.registerCallback<0>([&r2](){r2.EventZeroHappened();});
+
+    cb.raiseEvent<0>();
+    cb.raiseEvent<1>(37,"Hello There");
+    cb.raiseEvent<2>(182,150);
+
+    std::cout << "pre";
+
+    cb.unregisterCallback<0>([&r2](){r2.EventZeroHappened();});
+
+    std::cout << "apres";
+
+    cb.raiseEvent<0>();
+}
---
+++ observable.h
@@ -0,0 +1,61 @@
+#ifndef OBSERVABLE_HPP
+#define OBSERVABLE_HPP
+
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+
+template<typename T, typename... U>
+size_t getAddress(std::function<T(U...)> f) {
+    typedef T(fnType)(U...);
+    fnType ** fnPointer = f.template target<fnType*>();
+    return (size_t) *fnPointer;
+}
+
+template<typename ... Funcs>
+class Observable
+{
+public:
+    using ObserverList = std::tuple<std::vector<std::function<Funcs>>...>;
+
+    template<unsigned int eventidx, typename Callable>
+    void registerCallback(Callable c)
+    {
+        std::get<eventidx>(observers).emplace_back(c);
+    }
+
+    template<unsigned int eventidx, typename Callable>
+    void unregisterCallback(Callable c)
+    {
+        std::cout << "unregister";
+        //std::vector<std::function<void()>>::iterator it = std::find(std::get<eventidx>(observers).begin(), std::get<eventidx>(observers).end(), c);
+        //auto it = std::find(std::get<eventidx>(observers).begin(), std::get<eventidx>(observers).end(), c);
+        for (auto it = std::get<eventidx>(observers).begin(); it != std::get<eventidx>(observers).end(); it++) {
+//            if (getAddress( c) == getAddress(*it))
+//            {
+//                std::cout << "erasing";
+//                std::get<eventidx>(observers).erase(it);
+//            }
+//            else {
+//                std::cout << "nope";
+//            }
+        }
+        //std::get<eventidx>(observers).erase(c);
+    }
+
+    template<unsigned int eventidx, typename ... T>
+    void raiseEvent(T ... args)
+    {
+        for(auto f: std::get<eventidx>(observers))
+        {
+            f(std::forward<T>(args)...);
+        }
+    }
+
+private:
+    ObserverList observers;
+};
+
+#endif // OBSERVABLE_HPP
